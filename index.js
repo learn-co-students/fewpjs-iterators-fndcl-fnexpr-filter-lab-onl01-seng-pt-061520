4acb2b301aac98a459fa69560473c0253c4efeaa
hopegipson <gipson.hope@gmail.com>
2021-01-08 00:50:25 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(drivers, stringOne){
+    let matching = drivers.filter(n => {
+        return n.toLowerCase() === stringOne.toLowerCase();
+      });
+      return matching
+}
+
+function fuzzyMatch(drivers, stringOne){
+    let matching = drivers.filter(n => {
+        return n.startsWith(stringOne);
+      });
+      return matching
+    }
+
+function matchName(drivers, stringOne){
+    let matching = drivers.filter(n => {
+        return n.name.toLowerCase()
+        === stringOne.toLowerCase();
+      });
+      return matching
+    }
+
