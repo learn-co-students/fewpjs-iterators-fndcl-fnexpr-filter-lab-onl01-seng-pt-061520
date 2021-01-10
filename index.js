7a46e9469e74623770c35f5a107c500b748868b0
Jessie <jssdnvn.com>
2021-01-10 08:55:58 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+   const matching = drivers.filter(driver => driver.toLowerCase() == string.toLowerCase())
+   return matching 
+}
+
+function fuzzyMatch(drivers, string) {
+   const matching = drivers.filter(match => match.toLowerCase().indexOf(string.toLowerCase()) === 0)
+   return matching
+}
+
+function matchName(drivers, string){
+   const matching = drivers.filter(match => match.name.toLowerCase() === string.toLowerCase())
+   return matching 
+}
