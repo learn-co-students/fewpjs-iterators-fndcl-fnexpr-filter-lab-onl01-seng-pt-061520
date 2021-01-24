8208ca22202965b11b4192da33a61e22d690185e
Jade Lynn Masker <jadelynn@masker.family>
2021-01-24 17:33:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, string) {
+	return drivers.filter(function(driver) {
+		return driver.toLowerCase() === string.toLowerCase();
+	});
+}
+
+function fuzzyMatch(drivers, string) {
+	return drivers.filter(function(driver) {
+		return driver.toLowerCase().indexOf(string.toLowerCase()) === 0;
+	});
+}
+
+function matchName(drivers, string) {
+	return drivers.filter(function(driver) {
+		return driver.name === string;
+	});
+}
